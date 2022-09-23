1b493023d2f80ff6c98775f482ba8446ff8a5564
Schmidt Erik <schmidterik661@gmail.com>
2022-09-23 12:27:39 +0300

    landing-page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./header/Header";
+import Main from "./main/main";
+import Footer from "./footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
